8ac5740f121ff77a680e0bfb355673456fa05b65
duxiaorantj@gmail.com <duxiaorantj@gmail.com>
2023-03-23 23:39:55 +0800

    rest_framework

---
+++ accounts/api/views.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets    #pip install djangorestframework
+from rest_framework import permissions
+from accounts.api.serializers import UserSerializer
+
+
